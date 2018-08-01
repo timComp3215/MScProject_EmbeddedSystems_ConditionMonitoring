02dad839a13e40f273641595df329b8cf044dd90
guite t.j. (tjg1g14) <tjg1g14@soton.ac.uk>
2018-08-01 16:29:32 +0100

    Frequency analysis in Matlab ready

--- uartFFTadc/SupportFiles/MEMS/experimentData.m
+++ uartFFTadc/SupportFiles/MEMS/experimentData.m
@@ -1,13 +1,15 @@
 clear all
 close all
 
+%subplot = @(m,n,p) subtightplot (m, n, p, [0.1 0.05], [0.1 0.1], [0.1 0.1]);
+
 %Include noise
 
 fs = 16384; %Sample frequency
 %T = 0.125/4; %Measurement time period
 T = 4096/fs;
 
-model_frequency = 95;
+model_frequency = 553;
 
 N = fs * T;
 
@@ -23,37 +25,72 @@ x = zeros(N, 1);
 
 model_x = zeros(N, 1);
 
-frequency = 95;
+frequency = 4095;
 
 filename = sprintf('t_%d_mma7361.csv', frequency);
+%filename = sprintf('t_random_mma7361.csv');
 x_mma = csvread(filename);
+F_mma = fft(x_mma);
 x_mma = (x_mma / 16384) * 3.3 / 0.206;
 filename = sprintf('t_%d_adxl354.csv', frequency);
+%filename = sprintf('t_random_adxl354.csv');
 x_adxl = csvread(filename);
+F_adxl = fft(x_adxl);%Matlab fft
 x_adxl = (x_adxl / 16384) * 3.3 / 0.1;
 filename = sprintf('f_%d_mma7361.csv', frequency);
+%filename = sprintf('f_random_mma7361.csv');
 f_mma = csvread(filename);
 f_mma = f_mma / 0.206;
 filename = sprintf('f_%d_adxl354.csv', frequency);
+%filename = sprintf('f_random_adxl354.csv');
 f_adxl = csvread(filename);
 f_adxl = f_adxl / 0.1;
+%f_adxl = (f_adxl / 16384)*3.3 / 0.1;
 
-figure;
-subplot(2, 1, 1);
+figure('Color', 'w',  'Position', [500 800 1500 500]);
+subplot(1, 2, 1);
 plot(t, x_mma);
-hold on 
+ylabel('Acceleration (g)')
+xlabel('Time (s)')
+legend('MEMS (A)')
+subplot(1, 2, 2);
 plot(t, x_adxl);
-legend('MMA7361', 'ADXL354');
-title_str = sprintf('%d Hz', frequency);
-title(title_str);
+legend('MEMS (B)');
+ylabel('Acceleration (g)')
+xlabel('Time (s)')
+%title_str = sprintf('%d Hz', frequency);
+%title(title_str);
 
-subplot(2, 1, 2);
+figure('Color', 'w', 'Position', [500 200 1500 500]) 
+subplot(1, 2, 1);
 plot(f, f_mma);
-hold on 
+ylabel('Magnitude')
+xlabel('Frequency (Hz)')
+legend('MEMS (A)')
+subplot(1, 2, 2);
 plot(f, f_adxl);
-legend('MMA7361', 'ADXL354');
+ylabel('Magnitude')
+xlabel('Frequency (Hz)')
+legend('MEMS (B)')
 
-fs = 1e3;
-t = 0:1/fs:1;
-x = [1 2]*sin(2*pi*[50 250]'.*t) + randn(size(t))/10;
-lowpass(x,150,fs)
+%F_adxl = fft(x_adxl);
+F_adxl = abs(F_adxl/N)/0.1;
+F_adxl = F_adxl(1:round(N/2));
+
+%F_mma = fft(x_mma);
+F_mma = abs(F_mma/N)/0.206;
+F_mma = F_mma(1:round(N/2));
+
+%{
+figure('Color', 'w', 'Position', [800 200 1500 500]) ;
+subplot(1, 2, 1);
+plot(f, F_mma);
+ylabel('Magnitude')
+xlabel('Frequency (Hz)')
+legend('MEMS (A)')
+subplot(1, 2, 2);
+plot(f, F_adxl);
+ylabel('Magnitude')
+xlabel('Frequency (Hz)')
+legend('MEMS (B)')
+%}
