b0766ef4da72ae732b0133617367705190eebe44
guite t.j. (tjg1g14) <tjg1g14@soton.ac.uk>
2018-09-02 19:04:26 +0100

    More images and files

--- uartFFTadc/EnergyTrace/EnergyAnalysis.m
+++ uartFFTadc/EnergyTrace/EnergyAnalysis.m
@@ -41,7 +41,7 @@ plot(t+2.75, current_Both, 'LineWidth', 1);
 %plot(t+5, current_Running, 'LineWidth', 1);
 plot(t+5.25, current_Running_LPM0, 'LineWidth', 1);
 
-legend('Base program', 'LPM0', 'Acceleromter only', 'Current Sensor only', 'Both sensors', 'Running with LPM0', 'Location', 'East')
+legend('Base program', 'LPM', 'Acceleromter only', 'Current Sensor only', 'Both sensors', 'Running with LPM', 'Location', 'SouthEast')
 
 ylabel('Current (mA)')
 xlabel('Time (s)')
@@ -59,10 +59,17 @@ plot(t, voltage_base)
 
 %%
 
-energy_base = str2double(Base.Energy_uJ_(:))/1000000;
-energy_LPM0 = str2double(LPM0.Energy_uJ_(:))/1000000;
-energy_Vib = str2double(Vib.Energy_uJ_(:))/1000000;
-energy_MCSA = str2double(MCSA.Energy_uJ_(:))/1000000;
-energy_Both = str2double(Both.Energy_uJ_(:))/1000000; 
+energy_base = str2double(Base.Energy_uJ_(end))/1000000;
+energy_LPM0 = str2double(LPM0.Energy_uJ_(end))/1000000;
+energy_Vib = str2double(Vib.Energy_uJ_(end))/1000000;
+energy_MCSA = str2double(MCSA.Energy_uJ_(end))/1000000;
+energy_Both = str2double(Both.Energy_uJ_(end))/1000000; 
 
 %%
+
+mean_current_base = mean(current_base);
+mean_current_LPM0 = mean(current_LPM0);
+mean_current_MCSA = mean(current_MCSA);
+mean_current_Vib = mean(current_Vib);
+mean_current_Both = mean(current_Both);
+mean_current_Running_LPM0 = mean(current_Running_LPM0);
